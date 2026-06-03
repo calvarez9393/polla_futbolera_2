ecd1532b3cb76e5e7acae44b01aa533c6e9de689
Cristian Alvarez <cristian.alvarezm@cun.edu.co>
2026-06-02 20:46:52 -0500

    Remove dist/ from git tracking, update .gitignore

--- backend/dist/server.js
+++
@@ -1,7 +0,0 @@
-import { app } from "./app.js";
-import { env } from "./config/env.js";
-import { startScheduler } from "./modules/sync/scheduler.js";
-app.listen(env.PORT, () => {
-    console.log(`Backend listening on ${env.PORT}`);
-});
-startScheduler();
